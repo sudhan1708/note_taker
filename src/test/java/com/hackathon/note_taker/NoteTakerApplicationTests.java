12859af7823e2716b7e53f7d5018d40395b45bf0
sudhan <sudhan1708@gmail.com>
2025-01-31 12:45:22 +0530

    Initial commit

---
+++ src/test/java/com/hackathon/note_taker/NoteTakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.note_taker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteTakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
